e209292d882e98d8a93ff4a55274e26eb2712dba
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-04-25 06:13:53 +0000

    solved by me

---
+++ 800/Buttons.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Buttons {
+    public static void main(String[] args) {
+        FastReader fr=new FastReader();
+        PrintWriter out=new PrintWriter(System.out);
+
+        int t=fr.nextInt();
+        // int t=1;
+
+        Solution solution=new Solution();
+
+        while (t-- > 0) {
+            //take input
+            int a=fr.nextInt();
+            int b=fr.nextInt();
+            int c=fr.nextInt();
+
+            //make call to execute the logic
+            solution.solve(a,b,c,out);
+        }
+
+        out.close();
+    }
+}
+
+class Solution {
+    //write logic here and print the result
+    public void solve(int a,int b,int c,PrintWriter out) {
+        a+=c%2;
+
+        if (a>b)
+            out.println("First");
+        else
+            out.println("Second");
+    }
+}
+
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br=new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st==null || !st.hasMoreElements()) {
+            try {
+                st=new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str="";
+        try {
+            str=br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return str;
+    }
+}

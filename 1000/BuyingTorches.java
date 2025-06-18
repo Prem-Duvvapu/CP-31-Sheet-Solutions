dda86281edafb897e61f9d08d85508e957e3ba81
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-06-18 20:33:50 +0000

    solved by me

---
+++ 1000/BuyingTorches.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+public class BuyingTorches {
+    public static void main(String[] args) {
+        FastReader fr=new FastReader();
+        PrintWriter out=new PrintWriter(System.out);
+
+        int t=fr.nextInt();
+        // int t=1;
+
+        Solution solution=new Solution();
+
+        while (t-- > 0) {
+            //take input
+            long x=fr.nextLong();
+            long y=fr.nextLong();
+            long k=fr.nextLong();
+
+            //make call to execute the logic
+            solution.solve(x,y,k);
+
+            //new line after test case ans
+            solution.sb.append("\n");
+        }
+
+        out.println(solution.sb);
+
+        out.close();
+    }
+}
+
+class Solution {
+    public static final int INT_MOD=1_000_000_007;
+    public static final long LONG_MOD=1_000_000_007L;
+
+    public StringBuilder sb=new StringBuilder();
+
+    //write logic here and print the result
+    public void solve(long x,long y,long k) {
+        long trade1=(k*(y+1)-1)/(x-1);
+        if ((k*(y+1)-1)%(x-1)!=0)
+            trade1++;
+        long trade2=k;
+
+        sb.append(trade1+trade2);
+    }
+}
+
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br=new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st==null || !st.hasMoreElements()) {
+            try {
+                st=new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str="";
+        try {
+            str=br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return str;
+    }
+}

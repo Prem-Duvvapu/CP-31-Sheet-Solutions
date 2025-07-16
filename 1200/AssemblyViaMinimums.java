4e4e3aa981c53bc6712d27d011fbff95029b326e
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-07-16 18:26:11 +0000

    referred video solution

---
+++ 1200/AssemblyViaMinimums.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+public class AssemblyViaMinimums {
+    public static void main(String[] args) {
+        FastReader fr=new FastReader();
+        PrintWriter out=new PrintWriter(System.out);
+
+        int t=fr.nextInt();
+        // int t=1;
+
+        Solution solution=new Solution();
+
+        while (t-- > 0) {
+            //take input
+            int n = fr.nextInt();
+            int m = (n*(n-1))/2;
+            int[] b = new int[m];
+
+            for (int i=0;i<m;i++)
+                b[i] = fr.nextInt();
+
+            //make call to execute the logic
+            solution.solve(n,b,m);
+
+            //new line after test case ans
+            solution.sb.append("\n");
+        }
+
+        out.println(solution.sb);
+
+        out.close();
+    }
+}
+
+class Solution {
+    public static final int INT_MOD=1_000_000_007;
+    public static final long LONG_MOD=1_000_000_007L;
+
+    public StringBuilder sb=new StringBuilder();
+
+    //write logic here and print the result
+    public void solve(int n,int[] b,int m) {
+        StringJoiner sj = new StringJoiner(" ");
+        Arrays.sort(b);
+        int j=n;
+
+        for (int i=0;i<m && j>0;i+=j) {
+            sj.add(Integer.toString(b[i]));
+            j--;
+        }
+        sj.add(Integer.toString(b[m-1]));
+
+        sb.append(sj);
+    }
+}
+
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br=new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st==null || !st.hasMoreElements()) {
+            try {
+                st=new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str="";
+        try {
+            str=br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return str;
+    }
+}

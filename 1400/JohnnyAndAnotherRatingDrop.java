ba447810dabbf088fbf605f592b4737da40eb31a
Prem-Duvvapu <prem.duvvapu@gmail.com>
2025-09-18 07:26:22 +0000

    referred video solution

---
+++ 1400/JohnnyAndAnotherRatingDrop.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+public class JohnnyAndAnotherRatingDrop {
+    public static void main(String[] args) {
+        FastReader fr=new FastReader();
+        PrintWriter out=new PrintWriter(System.out);
+
+        int t=fr.nextInt();
+//        int t=1;
+
+        Solution solution=new Solution();
+
+        while (t-- > 0) {
+            //take input
+            long n = fr.nextLong();
+
+            //make call to execute the logic
+            solution.solve(n);
+
+            //new line after test case ans
+            solution.sb.append("\n");
+        }
+
+        out.println(solution.sb);
+
+        out.close();
+    }
+}
+
+class Solution {
+    public static final int INT_MOD=1_000_000_007;
+    public static final long LONG_MOD=1_000_000_007L;
+
+    public StringBuilder sb=new StringBuilder();
+
+    //write logic here and print the result
+    public void solve(long n) {
+        long res = 0;
+
+        for (long i=0;i<62;i++) {
+            if (((n >> i) & 1) == 1)
+                res += ((long)2 * ((long)1 << i) - 1);
+        }
+
+        sb.append(res);
+    }
+}
+
+class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+        br=new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+        while (st==null || !st.hasMoreElements()) {
+            try {
+                st=new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return st.nextToken();
+    }
+
+    int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+        String str="";
+        try {
+            str=br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return str;
+    }
+}
